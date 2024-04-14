ec0b995b2039f7c23f2799e2ff53de849513df3c
lujuste <lu.scatollini@hotmail.com>
2024-04-14 13:46:13 -0300

    feature: end home page

--- src/shared/components/Footer/index.tsx
+++ src/shared/components/Footer/index.tsx
@@ -1,12 +1,80 @@
 import React from "react";
 
-import { Container, EndFooter } from "./styles";
+import {
+  BoxContacts,
+  BoxLogo,
+  Container,
+  EndFooter,
+  MaxWidth,
+  List,
+  TotalRightBox,
+} from "./styles";
+import Logo from "@/assets/logo-white.svg";
+import Image from "next/image";
+import redLogo from "@/assets/Modo_de_isolamento.svg";
 
 const Footer: React.FC = () => {
   return (
     <>
-      <Container></Container>
-      <EndFooter></EndFooter>
+      <Container>
+        <MaxWidth>
+          <BoxLogo>
+            <Image
+              src={Logo}
+              quality={100}
+              alt="logo-imobiliaria-portfolio"
+              unoptimized
+            />
+
+            <h3>CRECI: 10930J || Política de Privacidade</h3>
+          </BoxLogo>
+
+          <BoxContacts>
+            <List>
+              <ul>
+                <li>
+                  <strong>Links úteis</strong>
+                </li>
+                <li>Encomende seu imóvel</li>
+                <li>Cadastre seu imóvel</li>
+                <li>Seja um parceiro</li>
+              </ul>
+            </List>
+            <List>
+              <ul>
+                <li>
+                  <strong>Financiamento</strong>
+                </li>
+                <li>Financiamento e bancos</li>
+              </ul>
+            </List>
+            <List>
+              <ul>
+                <li>
+                  <strong>Contato</strong>
+                </li>
+                <li>Fale conosco</li>
+              </ul>
+            </List>
+          </BoxContacts>
+        </MaxWidth>
+      </Container>
+      <EndFooter>
+        <p>2024 © Copyright Portfolio imóveis</p>
+
+        <TotalRightBox>
+          <p>Desenvolvido por:</p>
+
+          <Image
+            src={redLogo}
+            quality={100}
+            width={19}
+            height={24}
+            alt="logo-imobiliaria-portfolio"
+            unoptimized
+          />
+        </TotalRightBox>
+      </EndFooter>
     </>
   );
 };
--- src/shared/components/Footer/styles.ts
+++ src/shared/components/Footer/styles.ts
@@ -14,4 +14,68 @@ export const EndFooter = styled.footer`
   background-color: #000;
   display: flex;
   background-color: #d71016;
+  padding: 0 50px;
+  align-items: center;
+  justify-content: center;
+
+  p {
+    font-size: 16px;
+    color: #fff;
+    font-weight: 300;
+  }
+`;
+
+export const TotalRightBox = styled.div`
+  display: flex;
+  margin-left: auto;
+  gap: 62px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const List = styled.div`
+  flex-direction: column;
+  display: flex;
+  min-width: 100px;
+  margin-left: 36px;
+`;
+
+export const BoxContacts = styled.div`
+  flex: 0.7;
+  display: flex;
+  margin-left: auto;
+  margin-top: 51px;
+
+  ul {
+    li {
+      font-size: 16px;
+      margin-top: 12px;
+      list-style: none;
+      color: #fff;
+      font-weight: 300;
+    }
+  }
+`;
+
+export const MaxWidth = styled.div`
+  max-width: 1366px;
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  display: flex;
+  padding: 0 20px;
+`;
+
+export const BoxLogo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 24px;
+
+  h3 {
+    font-size: 16px;
+    color: #c4c4c4;
+    margin-left: 2.6rem;
+  }
 `;
