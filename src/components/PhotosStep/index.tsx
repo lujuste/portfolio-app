f9c23757981f7ecb00a1bc44dc53280d9d3841f1
lucas juste <lucas.juste.3@globalhitss.com.br>
2024-05-15 10:42:06 -0300

    new page of steps finished all

--- src/components/PhotosStep/index.tsx
+++ src/components/PhotosStep/index.tsx
@@ -1,9 +1,52 @@
 import React from "react";
 
-import { Container } from "./styles";
+import {
+  ButtonAttachment,
+  ButtonIcon,
+  Container,
+  GridContainer,
+  GridItem,
+  HStack,
+} from "./styles";
+import Image from "next/image";
+import iconAttachment from "@/assets/anexo-icon.png";
+import exampleImage from "@/assets/imagemexemplo.png";
 
 const PhotosStep: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <h3>Precisamos de algumas fotos</h3>
+      <HStack>
+        <ButtonAttachment>
+          <h3>Anexe suas fotos aqui</h3>
+        </ButtonAttachment>
+        <ButtonIcon>
+          <Image alt="" width={18} height={18} src={iconAttachment} />
+        </ButtonIcon>
+      </HStack>
+
+      <GridContainer>
+        <GridItem>
+          <Image alt="" width={50} height={50} src={exampleImage} />
+        </GridItem>
+        <GridItem>
+          <Image alt="" width={50} height={50} src={exampleImage} />
+        </GridItem>
+        <GridItem>
+          <Image alt="" width={50} height={50} src={exampleImage} />
+        </GridItem>
+        <GridItem>
+          <Image alt="" width={50} height={50} src={exampleImage} />
+        </GridItem>
+        <GridItem>
+          <Image alt="" width={50} height={50} src={exampleImage} />
+        </GridItem>
+        <GridItem>
+          <Image alt="" width={50} height={50} src={exampleImage} />
+        </GridItem>
+      </GridContainer>
+    </Container>
+  );
 };
 
 export default PhotosStep;
--- src/components/PhotosStep/styles.ts
+++ src/components/PhotosStep/styles.ts
@@ -1,3 +1,63 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+
+  h3 {
+    font-size: 14px;
+    font-weight: 500;
+    font-family: Inter, sans-serif;
+  }
+`;
+
+export const HStack = styled.section`
+  display: flex;
+  width: 100%;
+  min-height: 40px;
+  height: 40px;
+  margin-top: 10px;
+`;
+
+export const ButtonAttachment = styled.button`
+  display: flex;
+  flex: 1;
+  min-height: 40px;
+  height: 40px;
+  border: 1px solid #979797;
+  padding: 0 10px;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const ButtonIcon = styled.button`
+  display: flex;
+  width: 100%;
+  min-width: 72px;
+  max-width: 72px;
+  min-height: 40px;
+  height: 40px;
+  background-color: #12100c;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+export const GridContainer = styled.div`
+  display: grid;
+  width: 100%;
+  height: auto;
+  /* background-color: brown; */
+  grid-template-columns: repeat(6, 1fr);
+  grid-column-gap: 32px;
+  margin: 42px 0 28px 0;
+`;
+
+export const GridItem = styled.div`
+  display: flex;
+  width: 72px;
+  height: 72px;
+  background-color: #e3e3e3;
+  align-items: center;
+  justify-content: center;
+`;
